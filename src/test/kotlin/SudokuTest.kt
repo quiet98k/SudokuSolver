352c7c6e5784cd919ca18f0e702912111b446544
quiet98k <58801358+quiet98k@users.noreply.github.com>
2024-03-23 05:23:51 -0700

    Initial commit

---
+++ src/test/kotlin/SudokuTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class SudokuTest {
+
+    @Test
+    operator fun iterator() {
+    }
+
+    @Test
+    fun isValid() {
+    }
+}
